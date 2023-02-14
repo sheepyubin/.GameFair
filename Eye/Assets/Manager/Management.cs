9c0650e8bb351a04ce51087fecd445a964585054
eunpyo0621 <90306267+eunpyo0621@users.noreply.github.com>
2023-02-14 18:11:39 +0900

    pu
    blic

--- Eye/Assets/Manager/Management.cs
+++ Eye/Assets/Manager/Management.cs
@@ -5,24 +5,52 @@ using UnityEngine;
 public class Management : MonoBehaviour
 {
     public static byte HP;          //캐릭터의 HP
-    public static bool LR;          //캐릭터의 좌,우
+    public static bool LR;          //캐릭터의 좌(TRUE),우(FALSE)
     public static Vector2 CharPos;  //캐릭터의 위치좌표
-    float CurPos;                   //과거좌표
-    float PrevPos;                  //현재좌표
+    float CurPos;                   //과거좌표.X
+    float PrevPos;                  //현재좌표.X
+
+    int stage1_monster;
+    bool Isstage1_boss;
+    [SerializeField] GameObject stage1_boss;
+    [SerializeField] GameObject stage1_wall;
+    
+    int stage2_monster;
+    bool Isstage2_boss;
+    [SerializeField] GameObject stage2_boss;
+    [SerializeField] GameObject stage2_wall;
+
+    int stahe3_monster;
+    bool Isstahe3_boss;
+    [SerializeField] GameObject stage3_boss;
+    [SerializeField] GameObject stage3_wall;
+
     void Start()
     {
-        HP = 100;
         CurPos = CharPos.x;
-    }
-    void LRcomparison()
-    {
-        if(CurPos< PrevPos)
-        {
+        HP = 100;
+        LR = false;
 
-        }
+        stage1_monster = 14;
+        stage2_monster = 16;
+        stahe3_monster = 19;
+
     }
     void Update()
     {
-        //if()
+        LRcomparison();
+
+    }
+    void LRcomparison()
+    {
+        PrevPos = CharPos.x;
+
+        if (CurPos < PrevPos) //과거의 X값보다 현재의 X값이 작다면(좌로 이동)
+        { LR = true; }
+
+        else if (CurPos > PrevPos) //과거의 X값보다 현재의 X값이 크다면(우로 이동)
+        { LR = false; }
+
+        CurPos = PrevPos;
     }
 }
