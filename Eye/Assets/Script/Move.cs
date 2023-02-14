fa0ef2945726e1a5d6b796b5dd70a26a6053b420
sheepyubin <yangyubinstory@naver.con>
2023-02-14 12:23:24 +0900

    콜라이더 끼는거 수정
    .

--- Eye/Assets/Script/Move.cs
+++ Eye/Assets/Script/Move.cs
@@ -2,7 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class Move : MonoBehaviour
 {
-    public float spped;
+    public float maxSpeed;// 속도
+    public float jumpPower; // 점프
+    public Vector2 Range;
+    bool isground;
+    [SerializeField] Transform pos;
+    [SerializeField] float radius;
+    [SerializeField] LayerMask layer;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Transform trans;
+    int jumpcount =2; //2단점프
+    int Jumpcnt;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Jumpcnt = jumpcount;
+
+    }
+
+    void Update()
+    {
+        isground = Physics2D.OverlapCircle(pos.position, radius, layer); //땅에 닿았는가?
+
+        if (isground == true && Input.GetKeyDown("c") && Jumpcnt > 0) //점프 1
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+        if (isground == false && Input.GetKeyDown("c") && Jumpcnt > 0) //점프 2
+        {
+            rigid.AddForce(Vector2.up * jumpPower * 0.7f, ForceMode2D.Impulse);
+
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            Jumpcnt--; //점프 카운트
+        }
+        if (isground)
+        {
+            Jumpcnt = jumpcount; //0이하로 내려가면 점프 불가
+        }
+        if (Input.GetButtonUp("Horizontal")) //속도제한
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, Range);
+        Gizmos.DrawWireSphere(pos.position, radius);
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse); //이동
+
+        if (rigid.velocity.x > maxSpeed)  
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+        else if (rigid.velocity.x < maxSpeed * (-1))  
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+    }
+
 }
